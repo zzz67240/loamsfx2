369307d5d194ab21c6b5eaf62bd526404e85fcc2
Xin Yang <zzz67240@gmail.com>
2021-08-11 00:24:08 +0800

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module loamsfx {
+	requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
